eafb7bfc937f28d7a357c73f6169eb209c742207
Morten berge <morten@berge.pro>
2021-03-09 04:55:28 +0100

    backend-frontend "tunnell"

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const fileUpload = require("express-fileupload")
 const Tesseract = require("tesseract.js")
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
 
 const app = express()
-
 app.use(fileUpload())
-
+app.use(cors());
 app.get("/", (_, res) => {
   res.send(`
     <form action='/upload' method='post' encType="multipart/form-data">
@@ -13,7 +15,9 @@ app.get("/", (_, res) => {
       <input type='submit' value='Upload!' />
     </form>`)
 })
-
+app.get("/res", (req,res) => {
+  res.send({message: "This message is from the backend :)"})
+})
 app.post("/upload", async (req, res) => {
   const { sampleFile } = req.files
   if (!sampleFile) return res.status(400).send("No files were uploaded.")
@@ -24,7 +28,8 @@ app.post("/upload", async (req, res) => {
     res.send(`<pre>${data.text}</pre>`)
   } catch (error) {
     throw error
+    res.send(error)
   }
 })
 
-app.listen(process.env.PORT || 3800)
+app.listen(port, () => console.log("Backend server live on " + port));
--- app/src/App.js
+++ app/src/App.js
@@ -2,12 +2,22 @@ import './App.css';
 import React from "react";
 import PublishIcon from '@material-ui/icons/Publish';
 import logo from "./logo.png";
+import Axios from "axios";
 
 function App() {
+   Axios({
+    method: "GET",
+    url: "http://localhost:5000/res",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(res => {
+    console.log(res.data.message);
+  });
   return (
     <div className="app">
         <div className="logo">
-          <img src={logo} />
+          <img src={logo} alt="" />
         </div>
         <div className="input__side">
             <div className="tekst__felt">
