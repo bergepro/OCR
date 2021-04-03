4855034c4e761052babadf82a9174af3e3fd4cc6
Morten berge <morten@berge.pro>
2021-04-03 12:23:19 +0200

    more features,now only react

---
+++ app/src/scripts/ImageLoader.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import ImageUploader from 'react-images-upload';
+import ClipLoader from "react-spinners/ClipLoader";
+import Tesseract from "tesseract.js";
+import "./ImageLoader.css";
+
+// https://stackoverflow.com/questions/48851807/how-to-get-jpg-image-from-heic-format-in-react-native 
+// ^ kanskje detta du ser itte.
+
+function ImageLoader() {
+    const [picUrl, setPicUrl] = useState([]); 
+    const [ocrText, setOcrText] = useState([]); 
+    const [isLoading, setIsLoading] = useState(false); 
+    
+    const onDrop = (_, pictureURL) => {
+        setPicUrl(pictureURL);
+        console.log("picurl: " + picUrl);
+        console.log("pictureURL: " + pictureURL);
+        
+    };
+    
+    const runOcr = () => {
+        picUrl.forEach((picture) =>
+         Tesseract.recognize(picture, "eng").then(({ data: { text } }) => {
+             setOcrText((oldarray) => [...oldarray, text]);
+         })
+        );
+        
+        setIsLoading(true);
+    };
+    return (
+        <div className="centered">
+            <ImageUploader 
+                className="button"
+                withIcon={false}
+                withPreview={true}
+                buttonText="Last opp fil(er)."
+                onChange={onDrop}
+                background="#6B4F9B"
+                imgExtension={[".jpg", ".jpeg", ".gif", ".png", ".gif"]}
+            />
+
+
+            <div className="ocr-button" onClick={runOcr}>
+                Skann tekst !
+            </div>
+            {ocrText.length > 0 ? (
+                <ul className="ocr-list">
+                    {ocrText.map((ot) => (
+                        <li className="ocr-element" key={ocrText.indexOf(ot)}>
+                            {ocrText.indexOf(ot) + 1}-) 
+                            {ot}
+                        </li>
+                    ))}
+                </ul>
+
+            ) : (
+                <ClipLoader color="#ffffff" loading={isLoading} size={150} />
+            )}
+        </div>
+    );
+}
+
+export default ImageLoader
